ac5e1a26578df05b033df7008d4aacecc6f8b989
Cullen179 <105689331+Cullen179@users.noreply.github.com>
2024-07-23 22:12:01 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      Dashboard
+    </div>
+  );
+}
