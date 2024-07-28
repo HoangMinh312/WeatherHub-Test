366fd3ccb34e84efe55c76e727c8355da7343ea6
Rainb0wBanana <117099173+Rainb0wBanana@users.noreply.github.com>
2024-07-28 20:22:12 +0700

    Create route.ts in send-email route
    This route.ts file will take in HTTP requests to the route /api/send-email. And sends the email to an array of emails.

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,40 @@
+import { EmailTemplate } from '../../../components/emailTemplates/email-templates';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET() {
+  try {
+
+    // can replace with actual weather data with fetch api from weather api
+    const weatherData = {
+      temperature: 25,
+      humidity: 80,
+      rainChance: 30,
+      UV: 5,
+    };
+
+    // can add more emails to the array
+    // can add more data to the object to send to the email template
+
+    const { data, error } = await resend.emails.send({
+      from: 'WeatherHub <onboarding@resend.dev>',
+      to: ['s3977773@rmit.edu.vn'],
+      subject: 'WeatherHub Alert!',
+      react: EmailTemplate({
+        temperature: weatherData.temperature,
+        humidity: weatherData.humidity,
+        rainChance: weatherData.rainChance,
+        UV: weatherData.UV,
+      }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
